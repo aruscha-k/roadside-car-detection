d9bd4b932154a9e6ddf2fea78dfc18c69ece67d8
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-02-06 14:39:39 +0100

    addad parameter parsing

--- python-scripts/nominatim_requests.py
+++ python-scripts/nominatim_requests.py
@@ -2,15 +2,20 @@ import requests
 
 
 # params: any String like "Volkmarsdorf" or even zipcodes like "04277"
-def request_search(params):
+def request_search(street, params):
     # url from webservice
     #URL = "https://nominatim.openstreetmap.org/search?q=" + params + "+Leipzig+Stadt&format=json"
-    
+
     # url from local service
-    URL = "http://localhost:8080/search?q=" + params + "+Leipzig+Stadt&format=json"
-    response = requests.get(URL)
+    request_url = "http://localhost:8080/search?q=" + street+ "+,Leipzig+Stadt&format=geojson"
+                                                                                                                                                                                                                                                        
+    if params:
+        for key, value in params.items():
+            request_url = request_url + '&' + str(key) + '=' + str(value)
+
+    response = requests.get(request_url)
     if response.status_code == 200:
-        return response
+        return response.json()
 
 #http://localhost:8080/search?format=geojson&q=Eisenbahnstr,leipzig&dedupe=0&addressdetails=1&extratags=1
 
