c7a47e9ca72102b8dc9b23f3379e363238c2e9c5
mo28taxu <mo28taxu@informatik.informatik.uni-leipzig.de>
2023-02-09 17:46:29 +0100

    reference suport

--- python-scripts/create_db_schema.py
+++ python-scripts/create_db_schema.py
@@ -23,19 +23,34 @@ def open_connection(config: dict):
     return connection
 
 
-def create_tables(connection, schema):
+def schema_to_sql(schema: dict) -> list[str]:
+    sql_list = []
+
+    tab_prim_key_dict = {tab['name']: [col['name'] for col in tab['columns'] if 'primary key' in col] for tab in schema}
+    tab_prim_key_dict = {k:v[0] for k,v in tab_prim_key_dict.items() if any(v)}
+
+    for table in schema:
+        non_attributes = ['name', 'type', 'references']
+        col_names = [col['name'] for col in table["columns"]]
+        col_datatypes = [col['type'] for col in table["columns"]]
+        col_attributes = [[k for k in col if k not in non_attributes] for col in table["columns"]]
+        col_attributes = [' '.join(a) if any(a) else None for a in col_attributes]
+        col_references = [f'references {col["references"]}({tab_prim_key_dict[col["references"]]})' if 'references' in col else None for col in table["columns"]]
+        
+        column_str_list = [' '.join([x for x in t if x is not None]) for t in zip(col_names, col_datatypes, col_attributes, col_references)]
+        column_str_list = [f'\t{col}' for col in column_str_list]
+        columns_str = ',\n'.join(column_str_list)
+        
+        sql_str = f'CREATE TABLE {table["name"]} (\n{columns_str})'
+        sql_list.append(sql_str)
+    
+    return sql_list
+
+def create_tables(connection, sql_list: list[str]):
     with connection.cursor() as cursor:
-        for table in schema:
-            columns_str_list = [f'{col["name"]} {col["data_type"]}' if all(k in ['name', 'data_type'] for k in col)
-                                else f'{col["name"]} {col["data_type"]} ' + ' '.join([k for k,v in col.items() if k not in ['name', 'data_type'] and v])
-                                for col in table["columns"]]
-            columns_str_list = ','.join(columns_str_list)
-            
-            sql_str = f'CREATE TABLE {table["name"]} ({columns_str_list})'
+        for sql_str in sql_list:
             print(f'to db: {sql_str}')
-
             cursor.execute(sql_str)
-
         connection.commit()
 
 
@@ -48,7 +63,9 @@ if __name__ == "__main__":
     schema_path = f'{res_folder_path}/{config["schema"]}'
     schema = hd.load_json(schema_path)
 
+    sql_list = schema_to_sql(schema)
+
     with open_connection(config) as conn:
-        create_tables(conn, schema)
+        create_tables(conn, sql_list)
 
     print('Done')
