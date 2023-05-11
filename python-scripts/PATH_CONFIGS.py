2d640d91548213f8955389f3280721bda12004e5
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-05-11 08:43:11 +0200

    minor path changes

--- python-scripts/PATH_CONFIGS.py
+++ python-scripts/PATH_CONFIGS.py
@@ -1,8 +1,9 @@
 RES_FOLDER_PATH = "./add-files"
 DATASET_FOLDER_PATH = "datasets/complete"
 
-IMG_FOLDER_PATH = "./imgs"
-CYCLO_IMG_FOLDER_PATH = IMG_FOLDER_PATH + "cyclomedia"
+IMG_FOLDER_PATH = "./imgs/"
+CYCLO_IMG_FOLDER_PATH = IMG_FOLDER_PATH + "cyclomedia/"
+AIR_IMG_FOLDER_PATH = IMG_FOLDER_PATH + "air/"
 
 DB_CONFIG_FILE_NAME = "db_config.json"
 LOAD_DATA_CONFIG_NAME = "load_data_config.json"
