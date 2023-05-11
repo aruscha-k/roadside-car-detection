5f78473068a608726824e40b2ba9d8756aa9e81d
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-05-11 11:47:58 +0200

    changed sql schema, added constraints for composite primary keys rewrote load sql schema into DB

--- python-scripts/DB_create_db_schema.py
+++ python-scripts/DB_create_db_schema.py
@@ -6,35 +6,37 @@ import DB_helpers as db_helper
 from PATH_CONFIGS import RES_FOLDER_PATH, DB_CONFIG_FILE_NAME
 
 
-def schema_to_sql(schema: dict) -> list[str]:
-    sql_list = []
+def create_tables(connection, tables):
 
-    tab_prim_key_dict = {tab['name']: [col['name'] for col in tab['columns'] if 'primary key' in col] for tab in schema}
-    tab_prim_key_dict = {k:v[0] for k,v in tab_prim_key_dict.items() if any(v)}
+    cursor = connection.cursor()
 
-    for table in schema:
-        non_attributes = ['name', 'type', 'references']
-        col_names = [col['name'] for col in table["columns"]]
-        col_datatypes = [col['type'] for col in table["columns"]]
-        col_attributes = [[k for k in col if k not in non_attributes] for col in table["columns"]]
-        col_attributes = [' '.join(a) if any(a) else None for a in col_attributes]
-        col_references = [f'references {col["references"]}({tab_prim_key_dict[col["references"]]})' if 'references' in col else None for col in table["columns"]]
-        
-        column_str_list = [' '.join([x for x in t if x is not None]) for t in zip(col_names, col_datatypes, col_attributes, col_references)]
-        column_str_list = [f'\t{col}' for col in column_str_list]
-        columns_str = ',\n'.join(column_str_list)
-        
-        sql_str = f'CREATE TABLE {table["name"]} (\n{columns_str})'
-        sql_list.append(sql_str)
-    
-    return sql_list
+    for table in tables:
+        table_name = table["name"]
+        columns = table["columns"]
+        constraints = table.get("constraints", {})
 
-def create_tables(connection, sql_list: list[str]):
-    with connection.cursor() as cursor:
-        for sql_str in sql_list:
-            print(f'to db: {sql_str}')
-            cursor.execute(sql_str)
-        connection.commit()
+        # Create the SQL statement for creating the table
+        sql = f"CREATE TABLE {table_name} ("
+        for column in columns:
+            column_name = column["name"]
+            column_type = column["type"]
+            sql += f"{column_name} {column_type}"
+            sql += ", "
+
+        # Add primary key constraint
+        primary_key_columns = constraints.get("primaryKey")
+        if primary_key_columns:
+            sql += f"PRIMARY KEY ({', '.join(primary_key_columns)})"
+
+        # Add not null constraints
+        not_null_columns = constraints.get("notNull")
+        if not_null_columns:
+            for column_name in not_null_columns:
+                sql += f", ALTER COLUMN {column_name} SET NOT NULL"
+
+        sql = sql.rstrip(", ") + ")"
+        cursor.execute(sql)
+
 
 # methodto call, if this file will be imported and run by another file.
 # in this case res_folder_path and config_file_name can be set. otherwise the standart files will be used
@@ -51,13 +53,11 @@ def main(res_folder_path:str=None, config_file_name:str=None) -> None:
     config_path = f'{res_folder_path}/{config_file_name}'
     config = db_helper.load_json(config_path)
 
-    schema_path = f'{res_folder_path}/{config["schema"]}'
-    schema = db_helper.load_json(schema_path)
-
-    sql_list = schema_to_sql(schema)
+    tables_schema_path = f'{res_folder_path}/{config["schema"]}'
+    tables = db_helper.load_json(tables_schema_path)
 
     with db_helper.open_connection(config, False) as con:
-        create_tables(con, sql_list)
+        create_tables(con, tables)
 
     print('create_db_schema Done')
 
