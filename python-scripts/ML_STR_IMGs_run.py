a786f140f90337e3f19319cbce6d0675bd36cc0b
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-06-02 08:27:33 +0200

    ML detection can now be done with one script for both image types when cuttign out air images, also save the coordinates for the sides of the road to DB

--- python-scripts/ML_STR_IMGs_run.py
+++ python-scripts/ML_STR_IMGs_run.py
@@ -1,12 +1,12 @@
 
 from DB_helpers import open_connection
-from PATH_CONFIGS import CYCLO_IMG_FOLDER_PATH, RES_FOLDER_PATH, DB_CONFIG_FILE_NAME, DB_USER
-from ML_STR_IMGs_methods import run_detection
+from PATH_CONFIGS import CYCLO_IMG_FOLDER_PATH, AIR_CROPPED_ROTATED_FOLDER_PATH, RES_FOLDER_PATH, DB_CONFIG_FILE_NAME, DB_USER
+from ML_IMGs_methods import run_detection
 
 import os
 import psycopg2
 
-def run(db_config, db_user, suburb_list):
+def run(db_config, db_user, suburb_list, img_type):
     with open_connection(db_config, db_user) as con:
         cursor = con.cursor()
 
@@ -25,7 +25,11 @@ def run(db_config, db_user, suburb_list):
             for i, segment_id in enumerate(segment_id_list):
                 print(f"------{i+1} of {len(segment_id_list)+1}, segment_ID: {segment_id}--------")
 
-                cursor.execute("""SELECT record_id, segmentation_number, order_number FROM segments_cyclomedia WHERE segment_id = %s ORDER BY segmentation_number ASC, order_number ASC""", (segment_id, ))
+                if img_type == "cyclo":
+                    cursor.execute("""SELECT record_id, segmentation_number, order_number FROM segments_cyclomedia WHERE segment_id = %s ORDER BY segmentation_number ASC, order_number ASC""", (segment_id, ))
+                elif img_type == "air":
+                    cursor.execute("""SELECT segmentation_number FROM segments_segmentation WHERE segment_id = %s ORDER BY segmentation_number ASC""", (segment_id, ))
+
                 result_rows = cursor.fetchall()
                 if result_rows == []:
                     print("no result for ", segment_id)
@@ -35,31 +39,41 @@ def run(db_config, db_user, suburb_list):
                     img_path_list = []
                     for idx, row in enumerate(result_rows):
 
-                        segmentation_number = result_rows[idx][1]
-                        recording_id = result_rows[idx][0]
-                        order_number = result_rows[idx][2]
+                        if img_type == "cyclo":
+                            segmentation_number = result_rows[idx][1]
+                            recording_id = result_rows[idx][0]
+                            order_number = result_rows[idx][2]
 
-                        if segmentation_number < 10:
-                            segmentation_number = "0" + str(segmentation_number)
-                        if order_number < 10:
-                            order_number = "0" + str(order_number)
+                            if segmentation_number < 10:
+                                segmentation_number = "0" + str(segmentation_number)
+                            if order_number < 10:
+                                order_number = "0" + str(order_number)
+                            img_file_path = CYCLO_IMG_FOLDER_PATH + str(segment_id)+ "_" + str(segmentation_number) + "_" + str(order_number) +  "__" + str(recording_id) + ".jpg"
 
-                        #read image and predict
-                        img_file_path = CYCLO_IMG_FOLDER_PATH + str(segment_id)+ "_" + str(segmentation_number) + "_" + str(order_number) +  "__" + str(recording_id) + ".jpg"
+                        elif img_type == "air":
+                            segmentation_number = result_rows[idx][0]
+                            img_file_path = AIR_CROPPED_ROTATED_FOLDER_PATH + str(ot_name) + "_" +  str(segment_id)+ "_" + str(segmentation_number) + ".tif"
+                        
                         if os.path.exists(img_file_path):
                             img_path_list.append(img_file_path)
                         else:
                             print("invalid path", img_file_path)
+                            break
+                    
+                    if img_path_list == []:
+                        continue
+                    parking_dict = run_detection(img_path_list, img_type)
 
-                    parking_dict = run_detection(img_path_list)
-
-                    #  write to DB
+                    #  write to DB # parking_dict = {'left': [()], 'right': []}
                     for key, value in parking_dict.items():
 
-                        for i in range(0, len(parking_dict[key])):
-                            db_key = "parking:" + key
+                        for (parking, percentage) in parking_dict[key]:
                             try:
-                                cursor.execute("""INSERT INTO tags VALUES (%s, %s, %s) """, (db_key, value[0], segment_id,))
+                                if img_type == "cyclo":
+                                    cursor.execute("""INSERT INTO parking_cyclomedia VALUES (%s, %s, %s, %s)""", (segment_id, key, parking, percentage,))
+                                elif img_type == "air":
+                                    cursor.execute("""INSERT INTO parking_air VALUES (%s, %s, %s, %s)""", (segment_id, key, parking, percentage,))
+
                             except psycopg2.errors.UniqueViolation as e:
                                 continue
                     con.commit()
@@ -68,8 +82,9 @@ def run(db_config, db_user, suburb_list):
 
 if __name__ == "__main__":
 
-    db_config_path = RES_FOLDER_PATH +"/"+ DB_CONFIG_FILE_NAME
-    run(db_config_path, DB_USER, [("Lindenau", 0)])
+    db_config_path = os.path.join(RES_FOLDER_PATH, DB_CONFIG_FILE_NAME)
+    run(db_config_path, DB_USER, [("Lindenau", 70)], img_type="cyclo")
+    run(db_config_path, DB_USER, [("Lindenau", 70)], img_type="air")
 
 
 #https://atlas.cyclomedia.com/PanoramaRendering/Render/WE4IK5SE/?apiKey=2_4lO_8ZuXEBuXY5m7oVWzE1KX41mvcd-PQZ2vElan85eLY9CPsdCLstCvYRWrQ5&srsName=epsg:55567837&direction=0&hfov=80
