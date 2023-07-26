af34f5a4175ad3d49518008befeeaf948eb7336f
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-07-26 17:39:17 +0200

    moved calculation of bounding box to helpers_geometry file added method to calculate lat,lon coordinates in geotif using pixel locatoin changed method to crop and rotate geotiffs; returns true or false if it was successfull changed the rotation method to use the same method as when rotating it back after object detection, therefore the e is no problem with added padding

--- python-scripts/AIR_IMGs_process.py
+++ python-scripts/AIR_IMGs_process.py
@@ -1,58 +1,22 @@
 from PIL import Image
 import rasterio
 import rasterio.mask
-import pickle
+from rasterio.transform import Affine
+import cv2
+
+import numpy as np
 import folium
 import math 
 from shapely import geometry
+from matplotlib import pyplot as plt
+from matplotlib import patches
 
 import PATH_CONFIGS as PATHS
-from helpers_geometry import calculate_slope, get_y_intercept, find_angle_to_x, calc_perpendicular, calc_interception_of_two_lines, calculate_start_end_pt
-from helpers_coordiantes import convert_coords, sort_coords
-
+from helpers_geometry import find_angle_to_x, calculate_start_end_pt
 
                 # lat, #lon
                 #316823.5441658454 5688228.192294073
 test_str_start = (51.3158290612, 12.3714331051)
-
-
-# given two points andthe wanted width calculate two parallel lines and perpendiculars
-# RETURNS:
-# bouding_box (list) with points in order [upper_left, upper_right, lower_right, lower_left] / [start_left, end_left, end_right, start_right]
-def calculate_bounding_box(str_pts, width):
-   
-    # slope and y intercept of original line, angle to x-axis of original line
-    m_origin = calculate_slope(str_pts)
-    b_origin = get_y_intercept(str_pts[0], m_origin) 
-    x_angle = find_angle_to_x(str_pts)
-
-    # calc perpendiculars for start point and endpoints
-    m_perpendicular, b_perpend_start, b_perpend_end = calc_perpendicular(str_pts)
-
-    # calc y intercept of parallel line: parallel line has slope of m_origin
-    new_width = width/2 * math.sqrt((m_origin**2) + 1)
-    b_parallel_below = (b_origin-new_width)
-    b_parallel_above = (b_origin+new_width)
-
-    print(f"original slope: {m_origin}, new width: {new_width}")
-
-    if abs(math.degrees(x_angle)) <= 45:
-
-        start_left = calc_interception_of_two_lines(m_perpendicular, b_perpend_start, m_origin, b_parallel_above) #upper left
-        end_left = calc_interception_of_two_lines(m_perpendicular, b_perpend_end, m_origin, b_parallel_above) #upper right
-        end_right = calc_interception_of_two_lines(m_perpendicular, b_perpend_end, m_origin, b_parallel_below) # lower right
-        start_right = calc_interception_of_two_lines(m_perpendicular, b_perpend_start, m_origin, b_parallel_below) #lower left
-    
-    elif abs(math.degrees(x_angle)) >= 45:
-
-        end_left = calc_interception_of_two_lines(m_perpendicular, b_perpend_end, m_origin, b_parallel_above) #upper left
-        end_right = calc_interception_of_two_lines(m_perpendicular, b_perpend_end, m_origin, b_parallel_below) #upper right
-        start_right = calc_interception_of_two_lines(m_perpendicular, b_perpend_start, m_origin, b_parallel_below) #lower right
-        start_left = calc_interception_of_two_lines(m_perpendicular, b_perpend_start, m_origin, b_parallel_above) #lower left
-
-    bounding_box = [start_left, end_left, end_right, start_right]
-    return bounding_box
-
 
 
 # method calculates the rotation angle, the image has to be rotated with, 
@@ -84,7 +48,8 @@ def cut_out_shape(bbox, out_tif, in_tif):
         try:
             out_image, out_transform = rasterio.mask.mask(src, [poly], crop=True)
             out_meta = src.meta
-        except ValueError:
+        except ValueError as e:
+            print(e)
             return False
 
     out_meta.update({"driver": "GTiff",
@@ -97,17 +62,116 @@ def cut_out_shape(bbox, out_tif, in_tif):
     return True
 
 
+# calculate lon, lat coordinates from pixel coordinates
+# PARAMS:
+#  img_path: path to the GEOTIFF
+#  x,y : pixel values to get lon, lat values for
+# RETURNS:
+#  lon, lat values
+def get_coordinates_from_px(img_path, x, y):
+    with rasterio.open(img_path) as src:
+        data = src.read()
+        # Get the geotransform parameters
+        transform = src.transform
+        # Apply the geotransform to get the geospatial coordinates from pixel values (row = y, columns = x)
+        lon, lat = transform * (x, y)
+
+    return lon, lat
+
+    
+def crop_and_rotate_geotiff(input_file, output_file, bbox, rotation_angle):
+    was_success = True
+    # Open the input GeoTIFF
+    with rasterio.open(input_file) as src:
+        # Define the geometry and transform for the cropping operation
+        poly = geometry.Polygon(bbox)
+        transform = src.transform
+
+        # Perform the crop; check for value error. can occur, when Streets overlap two districts
+        try:
+            cropped, out_transform = rasterio.mask.mask(src, [poly], crop=True)
+        except ValueError:
+            print("[!] Input shapes do not overlap raster.")
+            was_success = False
+            return was_success
+
+         # Save the cropped GeoTIFF (Optional)
+        cropped_meta = src.meta.copy()
+        cropped_meta.update({"driver": "GTiff",
+                             "height": cropped.shape[1],
+                             "width": cropped.shape[2],
+                             "transform": out_transform})
+
+        # Specify the output file for the cropped image
+        cropped_output_file = PATHS.AIR_TEMP_CROPPED_FOLDER_PATH + output_file
+
+        with rasterio.open(cropped_output_file, "w", **cropped_meta) as cropped_dest:
+            cropped_dest.write(cropped)
+
+        # Rotate the cropped image by the specified angle
+        # Convert the cropped image to a PIL Image object
+        cropped_image = Image.fromarray(cropped.transpose(1, 2, 0)) 
+
+        # Rotate the cropped image by the specified angle
+        rotated_image = rotate_with_matrix(cropped_image, rotation_angle)
+        #rotated_image = rotate_img(cropped_image, rotation_angle)
+
+        # Convert the rotated image back to a numpy array
+        rotated_image = np.array(rotated_image).transpose(2, 0, 1)
+
+
+        # Update the transformation matrix to include rotation
+        rotation_transform = Affine.rotation(rotation_angle)
+        updated_transform = rotation_transform * out_transform
+
+        # Update the metadata for the rotated GeoTIFF
+        out_meta = src.meta.copy()
+        out_meta.update({"driver": "GTiff",
+                         "height": rotated_image.shape[1],
+                         "width": rotated_image.shape[2],
+                         "transform": updated_transform})
+
+        rotated_output_file = PATHS.AIR_CROPPED_ROTATED_FOLDER_PATH + output_file
+        # Save the rotated GeoTIFF
+        with rasterio.open(rotated_output_file, "w", **out_meta) as dest:
+            dest.write(rotated_image)
+
+        return was_success
+
+
+# TODO: old - delete?
 # rotate the image for training with neural network, so that all images have 90degrees rotation from x axis (anticloclwise)
 # save to new file
-def rotate_img_only(in_tif_folder, out_tif_folder, file_name, angle):
-    
-    print(f"rotation angle: {angle}")
-    img = Image.open(in_tif_folder + file_name)
-    #expand 1 is used to keep the image from beeing cropped when rotated >90
-    rotate_img = img.rotate(angle=angle, expand=1)
-    #rotate_img.show(out_tif)
-    rotate_img.save(out_tif_folder + file_name)    
+# def rotate_img(image, angle):
+#     print(f"rotation angle: {angle}")
+#     #expand 1 is used to keep the image from beeing cropped when rotated >90
+#     rotated_img = image.rotate(angle=angle, expand=1)
 
+#     #rotate_img.save(out_tif_folder + file_name)    
+#     return rotated_img
+
+
+# calculate rotation matrix by using center and an angle in degrees; 
+# PARAMS:
+#  image: the to be rotated image as PIL image
+#  angle_degrees: the rotation angle in degrees
+# RETURNS;
+#  rotated_image: the rotated image as numpy array
+def rotate_with_matrix(image, angle_degrees):
+    image = np.array(image)
+    # Get the height and width of the cropped image
+    height, width = image.shape[:2]
+    center = (width / 2, height / 2)
+    
+    # Get the rotation matrix
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle_degrees, 1.0)
+    # Perform the rotation using warpAffine
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+
+    if len(rotated_image.shape) == 2:
+        rotated_image = cv2.cvtColor(rotated_image, cv2.COLOR_GRAY2RGB)
+    
+    return rotated_image
 
 
 
@@ -140,65 +204,45 @@ def plot_line(str: list, bbox: list):
 
     map_leipzig.save('index.html')
 
+
+# debugging method to plot the iteration poly and the points on a geotif
+def draw_on_geotiff(geotiff_path, pts, iter_poly):
+    pts, classes = map(list, zip(*pts))
+    # Open the GeoTIFF file using rasterio
+    with rasterio.open(geotiff_path) as dataset:
+    
+        # Read the data and the corresponding geographic information
+        data = dataset.read(1)  # Assuming it's a single-band raster (change index if needed)
+        transform = dataset.transform
+
+
+        # Print the boundaries
+        # Get the corner coordinates in lat/lon
+        # lon_min, lat_min = transform * (0, 0)
+        # lon_max, lat_max = transform * (dataset.width, dataset.height)
+        # print("Min Latitude:", lat_min)
+        # print("Max Latitude:", lat_max)
+        # print("Min Longitude:", lon_min)
+        # print("Max Longitude:", lon_max)
+
+        # Plot the GeoTIFF data
+        plt.imshow(data, extent=[transform[2], transform[2] + transform[0] * dataset.width,
+                                 transform[5] + transform[4] * dataset.height, transform[5]], cmap='viridis')
+        plt.colorbar()
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.title('GeoTIFF with Points')
+
+        # Plot the points on the same plot as the GeoTIFF
+        lon, lat = zip(*pts)  # Assuming 'pts' is a list of (longitude, latitude) tuples
+        plt.scatter(lon, lat, color='red', s=5)
+        polyx, polyy = zip(*iter_poly)
+        plt.scatter(polyx, polyy, color="blue", s=8)
+        plt.show()
+
+
+
 # ---------- end helper methods for debugging ------------------ #
 
 
-if __name__ == "__main__":
-
-    # the TIF-file to cut out from
-    tif_file = PATHS.testset + "Südvorstadt_TDOP_2022_RGB_10cm.tif"
-
-    # segment data from Leipzig as pandas DF
-    with open(PATHS.testset + "testset_segments_df.pkl", 'rb') as f:
-        segments_df = pickle.load(f)
-
-    # folders to save IMGs in
-    cropped_folder = PATHS.img_folder + 'cropped_imgs/'
-    rotated_folder = PATHS.img_folder + 'cropped_rotated/'
-
-#   #iterate city data and get image for each segment
-    # leave out streets of category "W"
-    for idx, row in segments_df.iterrows():
-
-        street_width = 20
-        if row['cat'] == "W":
-            continue
-       
-        print("--------", idx, str(row['object_id']))
-        coords = row['coords']
-
-        # if more than two coordinates, street has a bend => 
-        # partition the segment further and extract every two pairs of coordinate:
-        # calc start and end point of the whole street and resort the coords
-        if len(coords) > 2: 
-            converted_coords = [convert_coords("EPSG:4326", "EPSG:25833", pt[0], pt[1]) for pt in coords]
-            str_start, str_end = calculate_start_end_pt(converted_coords)
-            sorted_coords = sort_coords(converted_coords, str_start)
-            
-            segmentation_counter = 1
-            for i in range(0,len(sorted_coords)):
-                
-                try:
-                    img_file_name = str(row['object_id']) + "_" + str(segmentation_counter) + ".tif"
-                    temp_coords = [sorted_coords[i], sorted_coords[i+1]]
-                    bbox = calculate_bounding_box(temp_coords, street_width)
-                    rotation_angle = get_rotation_angle_for_img(temp_coords)
-                    cut_out_success = cut_out_shape(bbox, cropped_folder + img_file_name, tif_file)
-                    if cut_out_success:
-                        rotate_img_only(cropped_folder, rotated_folder, img_file_name, rotation_angle)
-                        segmentation_counter += 1
-                except IndexError:
-                    break
-        else:
-                            
-            converted_coords = [convert_coords("EPSG:4326", "EPSG:25833", pt[0], pt[1]) for pt in coords]
-            str_start, str_end = calculate_start_end_pt(converted_coords)
-            bbox = calculate_bounding_box([str_start, str_end], street_width)
-            rotation_angle = get_rotation_angle_for_img([str_start, str_end])
-            cut_out_success = cut_out_shape(bbox, cropped_folder + str(row['object_id']) + ".tif", tif_file)
-            if cut_out_success:
-                rotate_img_only(cropped_folder, rotated_folder, str(row['object_id']) + ".tif", rotation_angle)
-            
-        # if idx == 1:
-        #     break
-
+#if __name__ == "__main__":
