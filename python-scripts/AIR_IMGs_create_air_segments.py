d3cd0ab872b3cfd3b9fa058c1b73774a97f93a77
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-07-27 17:06:37 +0200

    creation of air segments revised: extracted calculation of "new width" to method because it is needed in other parts as well added try except for calculation of sides => if existing continue added try / except when cutting from geotifs, when overlapping two districts => error because images are district wise

--- python-scripts/AIR_IMGs_create_air_segments.py
+++ python-scripts/AIR_IMGs_create_air_segments.py
@@ -1,7 +1,10 @@
+import ERROR_CODES as ec
+from GLOBAL_VARS import ITERATION_LENGTH
 from DB_helpers import open_connection
-from PATH_CONFIGS import RES_FOLDER_PATH, DB_CONFIG_FILE_NAME, AIR_TEMP_CROPPED_FOLDER_PATH, AIR_CROPPED_ROTATED_FOLDER_PATH, DATASET_FOLDER_PATH, DB_USER
-from AIR_IMGs_process import calculate_bounding_box, get_rotation_angle_for_img, cut_out_shape, rotate_img_only
+from PATH_CONFIGS import RES_FOLDER_PATH, DB_CONFIG_FILE_NAME, AIR_CROPPED_ROTATED_FOLDER_PATH, DATASET_FOLDER_PATH, DB_USER
+from AIR_IMGs_process import get_rotation_angle_for_img, crop_and_rotate_geotiff
 from helpers_coordiantes import convert_coords
+from helpers_geometry import calculate_bounding_box, calculate_specific_street_width
 
 import json
 from psycopg2 import errors
@@ -12,10 +15,12 @@ def write_bbox_to_DB(bbox, cursor, segment_id, segmentation_number):
     right_coords = [bbox[3], bbox[2]]
     left_coords = [convert_coords("EPSG:25833", "EPSG:4326", coord[0], coord[1]) for coord in left_coords]
     right_coords = [convert_coords("EPSG:25833", "EPSG:4326", coord[0], coord[1]) for coord in right_coords]
-
-    cursor.execute("""
+    try:
+        cursor.execute("""
                     INSERT INTO segments_segmentation_sides 
                     VALUES (%s, %s, %s, %s)""", (segment_id, segmentation_number, json.dumps(left_coords), json.dumps(right_coords), ))
+    except:
+        return
 
 
 # suburb_list = [(ot_name, ot_nr), ..]
@@ -45,12 +50,24 @@ def create_air_segments(db_config_path, db_user, suburb_list):
             for i, segment_id in enumerate(segment_id_list):
                 print(f"------{i+1} of {len(segment_id_list)+1}, segment_ID: {segment_id}--------")
 
-                # check if information is valid
-                cursor.execute("""SELECT segmentation_number FROM segments_segmentation WHERE segment_id = %s""", (segment_id, ))
-                segmentation_no = cursor.fetchall()
-                if len(segmentation_no) == 1 and segmentation_no[0][0] == -1:
-                    print("[!] information invalid - skip")
+                 #get segment information
+                cursor.execute("""SELECT * FROM segments_segmentation WHERE segment_id = %s ORDER BY segmentation_number""", (segment_id, ))
+                segmentation_result_rows = cursor.fetchall()
+
+                if segmentation_result_rows == []:
+                    print("NO RESULT FOR ID %s - skip!", segment_id)
                     continue
+
+                # check if information is valid
+                segmentation_no = segmentation_result_rows[0][1]
+                if len(segmentation_result_rows) == 1:
+                    segmentation_no = segmentation_result_rows[0][1]
+                    if segmentation_no == ec.WRONG_COORD_SORTING:
+                        #TODO
+                        # rec_IDs = [{'recording_id': ec.WRONG_COORD_SORTING, 'street_point': (0,0), 'recording_point': (0,0), 'recording_year': 0}]
+                        # load_into_db(rec_IDs=rec_IDs, segment_id=segment_id, segmentation_number=segmentation_no, connection=con)
+                        print("[!] no segmentation information - skip")
+                        continue
 
                 # get width of street segment
                 cursor.execute("""SELECT median_breite FROM trafficareas WHERE segm_gid = %s""", (segment_gid_list[i], ))
@@ -63,44 +80,34 @@ def create_air_segments(db_config_path, db_user, suburb_list):
                     print("multiple areas . skip")
                     continue
                 else:
-                    median_breite = median_breite[0][0] + (1/3*median_breite[0][0] )
-    
-                #get segment information
-                cursor.execute("""SELECT * FROM segments_segmentation WHERE segment_id = %s ORDER BY segmentation_number""", (segment_id, ))
-                segmentation_result_rows = cursor.fetchall()
-
-                if segmentation_result_rows == []:
-                    print("NO RESULT FOR ID %s", segment_id)
-                    continue
+                    median_breite = calculate_specific_street_width(median_breite[0][0])
+               
                 #check if the data already exists in folder or in tag table?: TODO
                 # else:
                     # if len(cyclo_result_rows) == len(segmentation_result_rows):
                     #     print("EXIST - SKIP")
                     #     continue
 
-                #cut out img:
+                # cut out img:
                 # segment is not divided into smaller parts
-                elif len(segmentation_result_rows) == 1:
+                if len(segmentation_result_rows) == 1:
+                                  
                     segmentation_number = segmentation_result_rows[0][1]
                     start_lat, start_lon = segmentation_result_rows[0][2], segmentation_result_rows[0][3]
                     end_lat, end_lon = segmentation_result_rows[0][4], segmentation_result_rows[0][5]
                     temp_coords = [(start_lat, start_lon), (end_lat, end_lon)]
-                    
-                    img_filename = str(ot_name) + "_" + str(segment_id) + "_" + str(segmentation_number)
+                    segment_img_filename = str(ot_name) + "_" + str(segment_id) + "_" + str(segmentation_number) + ".tif"
 
                     #bbox = [start_left, end_left, end_right, start_right]
                     bbox = calculate_bounding_box(temp_coords, median_breite)
-                    # write bbox data to DB for visualisation
+                    # write bbox data to DB for visualisation 
                     write_bbox_to_DB(bbox, cursor, segment_id, segmentation_number)
                     con.commit()
 
                     rotation_angle = get_rotation_angle_for_img(temp_coords)
-                    cut_out_success = cut_out_shape(bbox, AIR_TEMP_CROPPED_FOLDER_PATH + img_filename + ".tif", in_tif)
-
-                    if cut_out_success:
-                        #rotate_img_only(cropped_folder, rotated_folder, str(row['object_id']) + ".tif", rotation_angle)
-                        rotate_img_only(AIR_TEMP_CROPPED_FOLDER_PATH, AIR_CROPPED_ROTATED_FOLDER_PATH, img_filename + ".tif", rotation_angle)
-                        #TODO: WRITE TO DB
+                    cut_out_success = crop_and_rotate_geotiff(in_tif, segment_img_filename, bbox, rotation_angle)
+                    if not cut_out_success:
+                        continue
 
                 # segment is divided into smaller parts
                 elif len(segmentation_result_rows) > 1:
@@ -109,7 +116,7 @@ def create_air_segments(db_config_path, db_user, suburb_list):
                         start_lat, start_lon = segmentation_result_rows[idx][2], segmentation_result_rows[idx][3]
                         end_lat, end_lon = segmentation_result_rows[idx][4], segmentation_result_rows[idx][5]
                     
-                        img_filename = str(ot_name) + "_" + str(segment_id) + "_" + str(segmentation_number)
+                        segment_img_filename = str(ot_name) + "_" + str(segment_id) + "_" + str(segmentation_number) + ".tif"
             
                         temp_coords = [(start_lat, start_lon), (end_lat, end_lon)]
                         bbox = calculate_bounding_box(temp_coords, median_breite)
@@ -117,13 +124,11 @@ def create_air_segments(db_config_path, db_user, suburb_list):
                         con.commit()
                     
                         rotation_angle = get_rotation_angle_for_img(temp_coords)
-                        cut_out_success = cut_out_shape(bbox, AIR_TEMP_CROPPED_FOLDER_PATH + img_filename + ".tif", in_tif)
-                        if cut_out_success:
-                            rotate_img_only(AIR_TEMP_CROPPED_FOLDER_PATH, AIR_CROPPED_ROTATED_FOLDER_PATH, img_filename + ".tif", rotation_angle)
-                            #TODO: WRITE TO DB
-                    
-
+                        cut_out_success = crop_and_rotate_geotiff(in_tif, segment_img_filename, bbox, rotation_angle)
+                        if not cut_out_success:
+                            continue
+                 
 
 if __name__ == "__main__":
     config_path = f'{RES_FOLDER_PATH}/{DB_CONFIG_FILE_NAME}'
-    create_air_segments(db_config_path=config_path, db_user=DB_USER, suburb_list=[('Lindenau',70)])
+    create_air_segments(db_config_path=config_path, db_user=DB_USER, suburb_list=[('Südvorstadt', '40'), ("Anger-Crottendorf", "22"), ("Lindenau", 70), ("Volkmarsdorf", 21), ("Zentrum-Südost", "02")])
