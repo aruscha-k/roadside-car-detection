3bb1f7d2e928f4441944ee86f2b66697e770cc49
Aruscha Kramm <kramm@informatik.uni-leipzig.de>
2023-07-27 17:11:53 +0200

    added path to new detection model for air images

--- python-scripts/PATH_CONFIGS.py
+++ python-scripts/PATH_CONFIGS.py
@@ -4,7 +4,8 @@ DB_CONFIG_FILE_NAME = "db_config.json"
 LOAD_DATA_CONFIG_NAME = "load_data_config.json"
 
 CYCLO_DETECTION_MODEL = "train2_streetmodel_150img_00025lr_1500bs.pth"
-AIR_DETECTION_MODEL = "airmodel_final_lr_0_002_BS_8_BSPI_512_ITER_1000_best_so_far.pth"
+#AIR_DETECTION_MODEL = "airmodel_final_lr_0_002_BS_8_BSPI_512_ITER_1000_best_so_far.pth"
+AIR_DETECTION_MODEL = "parking_air_LR00023_Maxiter20000_BS3500_totalloss02099.pth"
 
 RES_FOLDER_PATH = "./add-files"
 
