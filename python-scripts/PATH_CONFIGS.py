d6af3534330cfe8c6e24afe8b6387eed0563131d
Jonas Kreusch <kreusch@informatik.uni-leipzig.de>
2023-09-07 15:21:02 +0200

    updated path_config to have different database modes updated readme with db setup instructions

--- python-scripts/PATH_CONFIGS.py
+++ python-scripts/PATH_CONFIGS.py
@@ -1,17 +1,37 @@
 import os
+import socket
 
-DB_CONFIG_FILE_NAME = "db_config.json"
-LOAD_DATA_CONFIG_NAME = "load_data_config.json"
+mode = 'Aruscha'
+if socket.gethostname() == 'Jonass-MacBook-Pro.local':
+    mode = 'server'
 
-CYCLO_DETECTION_MODEL = "train2_streetmodel_150img_00025lr_1500bs.pth"
-AIR_DETECTION_MODEL = "airmodel_final_lr_0_002_BS_8_BSPI_512_ITER_1000_best_so_far.pth"
+if mode == 'server':
+    DB_CONFIG_FILE_NAME = "db_server_config.json"
+    LOAD_DATA_CONFIG_NAME = "load_data_config.json"
 
-RES_FOLDER_PATH = "./add-files"
+    CYCLO_DETECTION_MODEL = "train2_streetmodel_150img_00025lr_1500bs.pth"
+    AIR_DETECTION_MODEL = "airmodel_final_lr_0_002_BS_8_BSPI_512_ITER_1000_best_so_far.pth"
 
-DATASET_FOLDER_PATH = "datasets/complete"
+    RES_FOLDER_PATH = "./add-files"
 
-DB_USER_ARUSCHA = os.path.join(RES_FOLDER_PATH, 'db_aruscha.json')
-DB_USER = DB_USER_ARUSCHA
+    DATASET_FOLDER_PATH = "datasets/complete"
+
+    DB_USER_ARUSCHA = os.path.join(RES_FOLDER_PATH, 'db_server_credentials.json')
+    DB_USER = DB_USER_ARUSCHA
+else:
+
+    DB_CONFIG_FILE_NAME = "db_config.json"
+    LOAD_DATA_CONFIG_NAME = "load_data_config.json"
+
+    CYCLO_DETECTION_MODEL = "train2_streetmodel_150img_00025lr_1500bs.pth"
+    AIR_DETECTION_MODEL = "airmodel_final_lr_0_002_BS_8_BSPI_512_ITER_1000_best_so_far.pth"
+
+    RES_FOLDER_PATH = "./add-files"
+
+    DATASET_FOLDER_PATH = "datasets/complete"
+
+    DB_USER_ARUSCHA = os.path.join(RES_FOLDER_PATH, 'db_aruscha.json')
+    DB_USER = DB_USER_ARUSCHA
 
 # ------ img resources --------#
 IMG_FOLDER_PATH = "./imgs/"
